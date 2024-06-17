207c3f43d762e2c6f6e1c1a956b84a73e19bd988
jnrwalter <walterjr_@outlook.com>
2024-06-17 09:49:59 -0300

    Listagem de arenas
    Foi implementada uma tela que lista as arenas disponíveis.

--- telas/arenas.php
+++ telas/arenas.php
@@ -1,19 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Arenas - Marca aí</title>
     <link rel="stylesheet" href="../css/arenas.css">
 </head>
+
 <body>
     <div class="container">
+
+        <a class="home" href="../index.html">Marca aí</a>
+
+        <div id="div-pesquisar">
+            <input type="text" name="" id="">
+            <button>Pesquisar</button>
+        </div>
+
         <h1>Arenas disponíveis</h1>
+
         <div class="card-container" id="card-container">
             <?php
             // Configurações de conexão com o banco de dados PostgreSQL
             $host = "localhost";
-            $port = "5432";
+            $port = "5434";
             $dbname = "marca_ai";
             $user = "postgres";
             $password = "post";
@@ -23,18 +34,22 @@
             try {
                 $pdo = new PDO($dsn);
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                
+
                 // Consulta SQL para selecionar todas as arenas
                 $sql = "SELECT id, nome, descricao, localizacao, capacidade FROM arenas";
                 $stmt = $pdo->query($sql);
 
                 // Exibindo as arenas em cards
                 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
                     echo '<div class="card">';
+                    echo '<a href="../telas/arena.php" class="link-arena">';
+                    echo '<img src="../imagens/logoApp.jpg">';
                     echo '<h2>' . htmlspecialchars($row["nome"]) . '</h2>';
                     echo '<p>' . htmlspecialchars($row["descricao"]) . '</p>';
                     echo '<p><strong>Localização:</strong> ' . htmlspecialchars($row["localizacao"]) . '</p>';
                     echo '<p><strong>Capacidade:</strong> ' . htmlspecialchars($row["capacidade"]) . ' pessoas</p>';
+                    echo '</a>';
                     echo '</div>';
                 }
 
@@ -48,4 +63,5 @@
     </div>
     <script src="script.js"></script>
 </body>
+
 </html>
